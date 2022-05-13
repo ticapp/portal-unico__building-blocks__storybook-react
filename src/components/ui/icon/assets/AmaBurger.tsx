468e45c0dfffc9c161cd1a7052cb717b555bf084
Ricardo Jesus <ricardo.jesus>
2022-05-13 16:59:42 +0100

    added props to svg icons

--- src/components/ui/icon/assets/AmaBurger.tsx
+++ src/components/ui/icon/assets/AmaBurger.tsx
@@ -6,6 +6,7 @@ export const component = (props: React.SVGProps<SVGSVGElement>) => {
       height="32"
       viewBox="0 0 32 32"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M28 6H4V8H28V6Z" />
       <path d="M28 24H4V26H28V24Z" />
