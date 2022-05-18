4c3ddd18f26eaceb0ac3528aaf09455249435034
CarolineRodrigues <31484196+CarolineRodrigues@users.noreply.github.com>
2022-05-18 17:59:15 +0100

    Jumbotron styled

--- src/components/ui/jumbotron/jumbotron.tsx
+++ src/components/ui/jumbotron/jumbotron.tsx
@@ -1,19 +1,44 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import classNames from 'classnames';
+import { v4 as uuidv4 } from 'uuid';
 
 export interface JumbotronProps {
   /** Add classes to the Jumbotron component */
   className?: string;
 
-  /** Inner childs of the component to be rendered */
-  children?: ReactNode;
+  /** Title of Jumbotron */
+  jumboTitle: Array<string>;
+
+  /**Description of title */
+  description?: string;
 }
 
-export const Jumbotron = ({ className, children, ...props }: JumbotronProps) => {
+export const Jumbotron = ({ className, jumboTitle, description, ...props }: JumbotronProps) => {
   const cssJumbotron = classNames('ama-jumbotron', className);
+
+  const renderTitle = () => {
+    return jumboTitle?.map((title, i) => {
+      if (jumboTitle.length - 1 === i) {
+        return (
+          <span className='h4-bold' key={uuidv4()}>
+            {title}
+          </span>
+        );
+      } else {
+        return (
+          <React.Fragment key={uuidv4()}>
+            <span className='h4-bold'>{title}</span>
+            <br />
+          </React.Fragment>
+        );
+      }
+    });
+  };
+
   return (
-    <Jumbotron className={cssJumbotron} {...props}>
-      {children}
-    </Jumbotron>
+    <div className={cssJumbotron} {...props}>
+      <h1 className='h4-bold'>{renderTitle()}</h1>
+      <p className='p-0'>{description}</p>
+    </div>
   );
 };
--- src/stories/components/ui/jumbotron/jumbotron.stories.tsx
+++ src/stories/components/ui/jumbotron/jumbotron.stories.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { ComponentMeta, Story } from '@storybook/react';
+import { Jumbotron, JumbotronProps } from '../../../../components/ui';
 
 export default {
   title: 'Components/Jumbotron',
   component: Jumbotron,
 } as ComponentMeta<typeof Jumbotron>;
 
-export const Jumbotron: Story<JumbotronProps> = () => {
-  return <></>;
+export const JumbotronBasic: Story<JumbotronProps> = (args) => {
+  return <Jumbotron {...args}></Jumbotron>;
 };
 
-Jumbotron.args = {} as JumbotronProps;
+JumbotronBasic.args = {
+  jumboTitle: ['Bem-vindo(a),', 'ao Balcão do Condutor'],
+  description: 'Consulte aqui todas as informações relevantes para si enquanto condutor',
+} as JumbotronProps;
 
-Jumbotron.storyName = 'Jumbotron';
+JumbotronBasic.storyName = 'Jumbotron';
