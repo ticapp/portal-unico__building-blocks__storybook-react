2fb271765cafe41865c239e39aca3676a6fc6fdf
Julio Machado <julio.machado@redit.pt>
2022-05-25 17:33:31 +0100

    add/ breadcrumb mobile

--- src/components/ui/breadcrumb/breadcrumb.tsx
+++ src/components/ui/breadcrumb/breadcrumb.tsx
@@ -1,10 +1,11 @@
 
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Icon } from '../icon';
 import './breadcrumb.scss';
 import classNames from 'classnames';
 import { v4 as uuidv4 } from 'uuid';
 import { Link } from '../link';
+import { useOutsideElementClick, useWindowSize } from '../../hooks';
 
 
 
@@ -23,25 +24,16 @@ export interface BreadCrumbProps {
     crumbSelectedUrl: string;
 }
 
-export const BreadCrumb = ({ className, breadcrumbs, crumbSelectedUrl }: BreadCrumbProps) => {
-
-
-    // [] Do all verifies will be use for all components
-    // [] Verify screen size and return different object (if is mobile, tablet or desktop)
-    // [] Get crumb url and name build link
-    // [] Test component in storybook
-
-
-    const cssBreadCrumb = classNames('ama-breadcrumb', className, 'd-flex align-items-center justify-content-start');
-
+const BreadCrumbDesktop = ({ className, breadcrumbs, crumbSelectedUrl }: BreadCrumbProps) => {
+    const cssBreadCrumbDesktop = classNames('ama-breadcrumb-desktop', className, 'd-flex align-items-center justify-content-start');
 
     return (
-        <div className={cssBreadCrumb}>
+        <div className={cssBreadCrumbDesktop}>
             {breadcrumbs.map((page, index) => {
 
                 const isSelected = page.url === crumbSelectedUrl ? 'selected' : '';
 
-                const linkClassNames = classNames('item', isSelected, 'py-8', 'px-0');
+                const linkClassNames = classNames('items', isSelected, 'py-8', 'px-0');
                 const iconContainerClassNames = classNames('icon-container', 'd-flex align-items-center justify-content-center mx-8');
 
                 const isAtualLink = index === breadcrumbs.length - 1;
@@ -57,7 +49,7 @@ export const BreadCrumb = ({ className, breadcrumbs, crumbSelectedUrl }: BreadCr
                         </Link>
                         {!isAtualLink &&
                             (
-                                <span className={iconContainerClassNames}>
+                                <span key={uuidv4()} className={iconContainerClassNames}>
                                     <Icon className='icon-style' icon='ama-chevron-right' />
                                 </span>
                             )
@@ -67,4 +59,65 @@ export const BreadCrumb = ({ className, breadcrumbs, crumbSelectedUrl }: BreadCr
             })}
         </div>
     )
+}
+
+const BreadCrumbMobile = ({ className, breadcrumbs, crumbSelectedUrl }: BreadCrumbProps) => {
+    const historyCrumbRef = useRef(null);
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    const cssBreadCrumbMobile = classNames('ama-breadcrumb-mobile', className, 'd-flex align-items-start justify-content-center flex-column');
+    const cssBreadCrumbTableHistoryButton = classNames('history-button', 'd-flex align-items-center justify-content-center');
+    const cssHistoryContainer = classNames('history-container', 'd-flex align-items-start justify-content-center flex-column mt-8 p-32');
+
+    const setIsOpenHandler = () => {
+        setIsOpen(!isOpen);
+    }
+
+    useOutsideElementClick(historyCrumbRef, () => setIsOpenHandler());
+
+    return (
+        <div className={cssBreadCrumbMobile}>
+            <p className='title mb-8'>Você está aqui:</p>
+
+            <button className={cssBreadCrumbTableHistoryButton} onClick={setIsOpenHandler}>
+                <Icon className='icon-style ' icon="ama-chevron-left" /> Histórico de infrações
+            </button>
+
+            {isOpen && (
+                <div ref={historyCrumbRef} className={cssHistoryContainer}>
+                    <ul>
+                        {breadcrumbs.map((page) => {
+
+                            return (
+                                <li key={uuidv4()}>
+                                    <Link className='items' link={page.url}>
+                                        {page.name}
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            )}
+
+
+        </div>
+    )
+}
+
+export const BreadCrumb = ({ className, breadcrumbs, crumbSelectedUrl }: BreadCrumbProps) => {
+
+
+    const { width } = useWindowSize();
+
+
+    if (width >= 1366) {
+        return <BreadCrumbDesktop className={className} breadcrumbs={breadcrumbs} crumbSelectedUrl={crumbSelectedUrl} />
+    }
+    else {
+        return <BreadCrumbMobile className={className} breadcrumbs={breadcrumbs} crumbSelectedUrl={crumbSelectedUrl} />
+    }
+
 }
