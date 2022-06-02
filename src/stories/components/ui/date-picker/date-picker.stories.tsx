335b59537b95e093e99ecc371f659daa18b1b9ee
Ricardo Jesus <ricardo.jesus@ticapp.gov.pt>
2022-06-02 10:59:22 +0000

    PU-162: Component Datepicker

---
+++ src/stories/components/ui/date-picker/date-picker.stories.tsx
@@ -0,0 +1,79 @@
+import { ComponentMeta, Story } from "@storybook/react";
+import React from "react";
+import {
+  DatePicker,
+  DatePickerProps,
+  DaysLabels,
+  MonthsLabels,
+  ModalAriaLabels
+} from "../../../../components/ui";
+
+export default {
+  title: "Components/Date Picker",
+  component: DatePicker,
+} as ComponentMeta<typeof DatePicker>;
+
+export const BasicDatePicker: Story<DatePickerProps> = () => {
+  return (
+    <>
+      <label id="date-picker-label">Escolha uma data:</label>
+      <DatePicker labeledBy="date-picker-label" />
+    </>
+  );
+};
+BasicDatePicker.storyName = "Basic Date Picker";
+
+export const CustomDatePicker: Story<DatePickerProps> = () => {
+  const date = new Date("1987-06-25T00:00:00.000Z");
+
+  const days = {
+    sunday: "Sunday",
+    monday: "Monday",
+    tuesday: "Tuesday",
+    wednesday: "Wednesday",
+    thursday: "Thursday",
+    friday: "Friday",
+    saturday: "Saturday",
+  } as DaysLabels;
+
+  const months = {
+    january: "January",
+    february: "February",
+    march: "March",
+    april: "April",
+    may: "May",
+    june: "June",
+    july: "July",
+    august: "August",
+    september: "September",
+    october: "October",
+    november: "November",
+    december: "December",
+  } as MonthsLabels;
+
+  const modalAriaLabels = {
+    title: "Pick a date",
+    description: "You can navigate the picker using the keyboard",
+    currentDay:"Day",
+    nextMonth:'Next month',
+    nextYear:'Next Year',
+    previousMonth:'Previous month',
+    previousYear:'Previous year'
+
+  } as ModalAriaLabels;
+
+  return (
+    <>
+      <label id="date-picker-label">Pick a date:</label>
+      <DatePicker
+        labeledBy="date-picker-label"
+        placeholder="dd-mm-yyyy"
+        date={date}
+        days={days}
+        months={months}
+        modalAriaLabels={modalAriaLabels}
+      />
+    </>
+  );
+};
+CustomDatePicker.storyName = "Custom Date Picker";
