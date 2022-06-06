410696686d7f03b3a8842a2ff186402819c345bb
Julio Machado <julio.machado@redit.pt>
2022-06-06 11:23:17 +0100

    fix/ eslint code style

--- src/stories/components/ui/status-bar/status-bar.stories.tsx
+++ src/stories/components/ui/status-bar/status-bar.stories.tsx
@@ -3,29 +3,25 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { StatusBar } from '../../../../components/ui/status-bar';
 
-
 export default {
-    title: 'Components/StatusBar',
-    component: StatusBar,
+  title: 'Components/StatusBar',
+  component: StatusBar
 } as ComponentMeta<typeof StatusBar>;
 
 export const BasicStatusBar: ComponentStory<typeof StatusBar> = (args) => {
-
-
-    return (
-        <BrowserRouter>
-            <StatusBar {...args} />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <StatusBar {...args} />
+    </BrowserRouter>
+  );
 };
 
 BasicStatusBar.storyName = 'StatusBar Example';
 
 BasicStatusBar.args = {
-    url: 'https://www.google.com',
-    title: 'Pontos da carta de condução',
-    subtitle: '15 pontos',
-    icon: 'ama-badge',
-    backButtonText: 'Voltar ao início',
-
-}
+  url: 'https://www.google.com',
+  title: 'Pontos da carta de condução',
+  subtitle: '15 pontos',
+  icon: 'ama-badge',
+  backButtonText: 'Voltar ao início'
+};
