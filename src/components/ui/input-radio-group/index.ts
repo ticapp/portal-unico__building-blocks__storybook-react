4bc44b50f8508d21639b896e81032f1ee7073188
Julio Machado <julio.machado@redit.pt>
2022-06-20 10:22:52 +0100

    fix/ code format

--- src/components/ui/input-radio-group/index.ts
+++ src/components/ui/input-radio-group/index.ts
@@ -1,2 +1,2 @@
-export {InputRadioGroup } from './input-radio-group';
-export type {InputRadioGroupProps } from './input-radio-group';
+export { InputRadioGroup } from './input-radio-group';
+export type { InputRadioGroupProps } from './input-radio-group';
