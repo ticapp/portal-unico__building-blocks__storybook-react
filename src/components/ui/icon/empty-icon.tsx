ff2bdee0718079af8c96c1aa563730e082ba34db
CarolineRodrigues <31484196+CarolineRodrigues@users.noreply.github.com>
2022-11-07 19:47:05 +0000

    svg accessibility

--- src/components/ui/icon/empty-icon.tsx
+++ src/components/ui/icon/empty-icon.tsx
@@ -1,7 +1,13 @@
-import React, { FC, SVGProps } from 'react';
+import * as React from 'react';
 
-export const EmptyIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-    <path fill="none" d="M0 0h24v24H0z" />
-  </svg>
-);
+export const EmptyIcon = (props: React.SVGProps<SVGSVGElement> | any) => {
+  const newProps = { ...props };
+  delete newProps.alt;
+  delete newProps.title;
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...newProps}>
+      <path fill="none" d="M0 0h24v24H0z" />
+    </svg>
+  );
+};
