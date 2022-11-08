90803b83cf82b43d83c3f7d73a1edcf625479b6f
CarolineRodrigues <31484196+CarolineRodrigues@users.noreply.github.com>
2022-11-08 14:51:09 +0000

    svg accessibility

--- src/components/ui/icon/empty-icon.tsx
+++ src/components/ui/icon/empty-icon.tsx
@@ -7,6 +7,7 @@ export const EmptyIcon = (props: React.SVGProps<SVGSVGElement> | any) => {
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...newProps}>
+      {props.title && <title>{props.title}</title>}
       <path fill="none" d="M0 0h24v24H0z" />
     </svg>
   );
